3c84fcc7fd9e605d1d5be0c390ac948f0100fc62
Vinicius Victor <viniciuslimavi05@gmail.com>
2022-08-24 20:55:08 -0300

    Formulário de usuários versão 0.1

---
+++ script.js
@@ -0,0 +1,42 @@
+//let res = document.querySelector("div#res");
+
+function formulario() {
+    const form = document.querySelector("#form");
+
+    function eventoForm(evento) {
+        evento.preventDefault();
+        const nome = form.querySelector("#nome");
+        const sobrenome = form.querySelector("#sobrenome");
+        const peso = form.querySelector("#peso");
+        const altura = form.querySelector("#altura");
+    }
+    form.addEventeListener("submit", eventoForm);
+}
+
+formulario();
+
+
+
+
+
+
+
+
+
+/*function criaUsu(nome, sobrenome, peso, altura) {
+    return [{nome, sobrenome, peso, altura}];
+}
+
+
+function adicionar() {
+    if ((nome.length == null) && (sobrenome.length == null) && (Number(peso.value) <= 0) && (Number(altura.value) <= 0)) {
+        window.alert("[ERROR] Preencha todos os campos!")
+    } else {
+        this.nome = nome.length;
+        this.sobrenome = sobrenome.length;
+        this.peso = Number(peso.value);
+        this.altura = Number(altura.value);
+    }
+}
+
+    //let dados = [{nome, sobrenome, peso, altura}]*/
